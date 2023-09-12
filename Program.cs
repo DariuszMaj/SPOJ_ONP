2303bdbe7c44bd848541b39e4661ed5ec67cd6db
Darek144 <91788770+DariuszMaj@users.noreply.github.com>
2023-09-12 18:51:10 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ONP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] result = new string[n];
+            for (int a=0;a<n;a++)
+            {
+                Stack<char> Mystack = new Stack<char>();
+                string line= Console.ReadLine();
+                for (int i=0; i < line.Length;i++)
+                {
+                    if(line[i]>='a' && line[i]<='z')
+                        result[a] += line[i].ToString();
+                    else if (line[i] == ')')
+                    {
+                        result[a] += Mystack.Peek().ToString();
+                        Mystack.Pop();
+                    }
+                    else if (line[i] != '(')
+                        Mystack.Push(line[i]);
+                }
+            }
+            foreach (var a in result)
+                Console.WriteLine(a);
+        }
+    }
+}
